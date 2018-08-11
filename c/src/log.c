c8729c3859ba31982c288cf43d2726f4cc84cbe7
Ian Fisher <iafisher@protonmail.com>
2018-08-10 22:02:08 -0400

    Add timestamp to log messages

--- c/src/log.c
+++ c/src/log.c
@@ -6,6 +6,7 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "log.h"
 
 
@@ -16,9 +17,13 @@ void set_logging_level(log_level_t new_level) {
     current_log_level = new_level;
 }
 
-
+#define TIME_BUF_LEN 20
 static void log_unconditional(const char* lvl, const char* fmt, va_list args) {
-    fprintf(stderr, "%s ", lvl);
+    time_t t = time(NULL);
+    char timebuf[TIME_BUF_LEN];
+    strftime(timebuf, TIME_BUF_LEN, "%Y-%m-%d %H:%M:%S", gmtime(&t));
+
+    fprintf(stderr, "[%s] %s,000: ", lvl, timebuf);
     vfprintf(stderr, fmt, args);
     fprintf(stderr, "\n");
 }
@@ -28,7 +33,7 @@ void log_info(const char* fmt, ...) {
     if (current_log_level <= LOG_LVL_INFO) {
         va_list args;
         va_start(args, fmt);
-        log_unconditional("[INFO]", fmt, args);
+        log_unconditional("INFO", fmt, args);
         va_end(args);
     }
 }
@@ -38,7 +43,7 @@ void log_fatal(const char* fmt, ...) {
     if (current_log_level <= LOG_LVL_CRITICAL) {
         va_list args;
         va_start(args, fmt);
-        log_unconditional("[CRITICAL]", fmt, args);
+        log_unconditional("CRITICAL", fmt, args);
         va_end(args);
         exit(2);
     }
